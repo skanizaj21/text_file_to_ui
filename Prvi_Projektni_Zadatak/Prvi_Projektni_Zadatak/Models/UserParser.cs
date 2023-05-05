b5938c3df07b33ba74e60c446f4e3512e07dea70
SebastianKanizaj <47847701+SebastianKanizaj@users.noreply.github.com>
2023-05-05 17:24:06 +0200

    Created a new class named UserParser

--- Prvi_Projektni_Zadatak/Prvi_Projektni_Zadatak/Models/UserParser.cs
+++ Prvi_Projektni_Zadatak/Prvi_Projektni_Zadatak/Models/UserParser.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace Prvi_Projektni_Zadatak.Models
 {
-    public class UserParser
+    internal class UserParser
     {
     }
 }
